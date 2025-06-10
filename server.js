67bb94fe1648f3bc80d9a3740c696343e8f78eda
Kimsungbin1 <ksb19558@naver.com>
2025-06-10 20:19:39 +0900

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ require("dotenv").config();
 const JWT_SECRET = process.env.JWT_SECRET || "your_jwt_secret"; // 반드시 환경 변수로 관리하세요
 const MONGO_URI =
   process.env.MONGO_URI || "mongodb://localhost:27017/betting_game";
-const FRONTEND_URL = process.env.FRONTEND_URL || "goldbac.netlify.app";
+const FRONTEND_URL = process.env.FRONTEND_URL || "https://goldbac.netlify.app";
 
 // Express 애플리케이션과 HTTP 서버 설정
 const app = express();
